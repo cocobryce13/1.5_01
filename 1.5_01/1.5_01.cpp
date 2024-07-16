0a7dc996edc5812f0712ea1d712675a3a5c057be
sokol <sokol@ASUS>
2024-07-16 19:17:07 +0500

    Добавьте файлы проекта.

---
+++ 1.5_01/1.5_01.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+
+
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int size = sizeof(arr) / sizeof(arr[0]);
+    
+	for (int i = 0; i < size; i++)
+	{
+		
+		if (i != size-1)
+		{
+			std::cout << arr[i] << ", ";
+		}
+		else
+		{
+			std::cout << arr[i] << std::endl;
+		}
+	} 
+
+	return 0;
+}
